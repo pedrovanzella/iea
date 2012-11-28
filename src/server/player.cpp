35ae340332bd9c8bf125f0c2daf35437556bbb94
Pedro Vanzella <pedro@pedrovanzella.com>
2012-11-28 16:38:29 -0200

    player has a numeric ID

--- src/server/player.cpp
+++ src/server/player.cpp
@@ -5,6 +5,12 @@ Player::Player()
 	_score = 0;
 }
 
+Player::Player(int id)
+{
+	_score = 0;
+	_id = id;
+}
+
 const int Player::score() const
 {
 	return _score;
@@ -18,4 +24,14 @@ int Player::score()
 void Player::increase_score()
 {
 	_score++;
+}
+
+const int Player::id() const
+{
+	return _id;
+}
+
+int Player::id()
+{
+	return _id;
 }
--- src/server/player.hpp
+++ src/server/player.hpp
@@ -5,14 +5,19 @@ class Player
 {
 	private:
 		int _score;
+		int _id;
 
 	public:
 		const int score() const;
 		int score();
 
+		const int id() const;
+		int id();
+
 		void increase_score();
 
 		Player();
+		Player(int id);
 };
 
 #endif /* _PLAYER_HPP_ */
