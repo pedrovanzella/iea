5907d56e77f2d8234680f5e13f9b73dae765fcad
Pedro Vanzella <pedro@pedrovanzella.com>
2012-12-04 13:50:11 -0200

    fuckety-fuck-fuck where is this segfault coming from?

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <boost/asio.hpp>
 #include "question.hpp"
+#include "server.hpp"
 #include "main.hpp"
 
 using std::cout;
 using std::cin;
+using std::cerr;
 using std::string;
 using std::endl;
 using std::vector;
 using std::ifstream;
 using std::getline;
+using std::atoi;
+using boost::asio::ip::tcp;
 
 int main(int argc, char** argv)
 {
-	cout << "Init server" << endl;
+	if (argc < 2) {
+		cerr << "Usage: " << argv[0] << " <port> [<port> ...]" << endl;
+		return 1;
+	}
+	cout << "Init server" << endl << endl;
 
 	vector<Question> questions;
 
 	cout << "Loading questions" << endl;
 	load_questions(questions);
+	cout << "Questions loaded" << endl;
 
-	for(vector<Question>::iterator i = questions.begin(); i != questions.end(); i++) {
-		cout << "Question: " << i->description() << endl << "\tAnswer: " << i->answer() << endl << endl;
+	//for(vector<Question>::iterator i = questions.begin(); i != questions.end(); i++) {
+	//    cout << "Question: " << i->description() << endl << "\tAnswer: " << i->answer() << endl << endl;
+	//}
+
+	boost::asio::io_service io_service;
+	server_list servers;
+	for (int i = 1; i < argc; ++i) {
+		tcp::endpoint endpoint(tcp::v4(), atoi(argv[i]));
+		server_ptr srvr(new server(io_service, endpoint));
+		servers.push_back(srvr);
 	}
+
+	io_service.run();
 
 	return EXIT_SUCCESS;
 }
